0646cae98c28cfd230385652be4a06ec24902300
Magdalena <menovska3@yahoo.com>
2020-08-05 19:13:02 +0200

    fragment initialized

--- app/src/main/java/com/gsixacademy/android/googlemapapp/MainActivity.kt
+++ app/src/main/java/com/gsixacademy/android/googlemapapp/MainActivity.kt
@@ -2,9 +2,13 @@ package com.gsixacademy.android.googlemapapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.gms.maps.model.MarkerOptions
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -21,5 +25,26 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
+
+        map.setOnMapLoadedCallback {
+            val gsixLocation = LatLng(42.011986, 21.372345)
+            val gtcLocation = LatLng(41.995422, 21.434676)
+            val dolnoSonje = LatLng(41.940128, 21.378703)
+            val builder = LatLngBounds.Builder()
+            builder.include(gsixLocation)
+            builder.include(gtcLocation)
+            builder.include(dolnoSonje)
+            val bounds = builder.build()
+            map.addMarker(MarkerOptions().position(gsixLocation).title("GSIX "))
+            // map.animateCamera(CameraUpdateFactory.newLatLngZoom(gsixLocation, 15f))
+            map.addMarker(MarkerOptions().position(gtcLocation).title("GTC"))
+            map.addMarker(MarkerOptions().position(dolnoSonje).title("Dolno Sonje"))
+            map.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 90))
+
+        }
+
+
+
+
     }
 }
